4b08da08f5fba4cf683715710d279cbdacb33df8
elieldiniz <elielfantichelle@outlook.com.br>
2023-08-15 13:25:01 -0400

    commit twe

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+//tipo literal nao pode receber nenhum outro valor
+
+
+//ex:
+//declaracao
+let literal: "hellow word"
+let pi: 3.14
+
+//atribuindo valor
+
+literal = "hi, loww" //ao atribuir valor diferente de hellow word retornara erro
+
+pi=20 //erro de atribuicao
+
+//ex: nesse caso o tipo literal pode ter 2 valores yes ou no, yes | no utilizando abarra
+//ajuda acriar tipos mais especificos
+
+let option: "yes" | "no"
+
+//ex: tambem pode diferenciar entre valores
+// assim pode se escolher entre number ou valor boolean
+
+let camada: number | boolean
+
+
+
+
+
+
+
+
